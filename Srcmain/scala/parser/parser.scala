1247349766f067bfc6a14dc0788ea552eb019cec
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-05-21 16:10:09 +0800

    Update parser.scala

--- Srcmain/scala/parser/parser.scala
+++ Srcmain/scala/parser/parser.scala
@@ -1,1 +1,7 @@
 package parser
+
+/** Implements a parser for the nodes language using scala's parser combinators.
+ *  Parsing is subdivided into separate lexing, postlexing and parsing phases.
+ *  - Lexing converts a String of nodes code into a List [ PreToken ]. PreTokens
+ *  are tokens where identifiers, ints and such carry the text that was lexed
+ *  to produce them.
