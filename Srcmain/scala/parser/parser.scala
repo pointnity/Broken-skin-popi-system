c4654c5ea2872e6772294acaf86e7490db6968b8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-05-22 20:52:55 +0800

    Update parser.scala

--- Srcmain/scala/parser/parser.scala
+++ Srcmain/scala/parser/parser.scala
@@ -138,3 +138,4 @@ object Parser extends Parsers {
 
   // Helper parser for server and receive to easily parse lists of
   // 'name: ty, ...' using repsep.
+  def nameTy: Parser [ ( Name , SType ) ] = name ~ COLON() ~ ty ^^ {
