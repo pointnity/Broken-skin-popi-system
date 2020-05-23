343dbe64dab3b8c62884bf74096f3287d1527ffc
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-05-23 19:23:05 +0800

    Update syntax.scala

--- Srcmain/scala/syntax/syntax.scala
+++ Srcmain/scala/syntax/syntax.scala
@@ -38,3 +38,4 @@ case object StdErrName extends PreDefName {
 object findNextName extends Function1[Set[Name], NumName] {
   def apply(names: Set[Name]): NumName =
       if ((names filter (_.isInstanceOf[NumName])).isEmpty) NumName ( 0 ) else
+    NumName ( ( names.map ( _.asInstanceOf[NumName].id ).max ) + 1 )
