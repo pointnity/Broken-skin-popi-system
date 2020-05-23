227d1f0a2fe865cceca8986c1f878b0ea15f3cda
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-05-23 19:17:44 +0800

    Update syntax.scala

--- Srcmain/scala/syntax/syntax.scala
+++ Srcmain/scala/syntax/syntax.scala
@@ -12,3 +12,4 @@ abstract class Name extends SyntaxElement
 
 case class NumName(val id: Int) extends Name {
   def next: NumName = new NumName(this.id + 1)
+  def pstr(names: Map[Name, String]): String =
