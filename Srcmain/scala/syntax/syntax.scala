6955e24f181dbe12a5e3bbdc08617c7c414e666e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-05-23 19:13:12 +0800

    Update syntax.scala

--- Srcmain/scala/syntax/syntax.scala
+++ Srcmain/scala/syntax/syntax.scala
@@ -4,3 +4,4 @@ import scala.util.parsing.input.Positional
 trait SyntaxElement {
   var info: Info = NoInfo
   def setInfo(i: Info): Unit = this.info = i
+  def pstr(names: Map[Name, String]): String
