7aba0f9d27091c9afc82c9cb9f4713fedd113c36
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-05-24 21:46:14 +0800

    Update syntax.scala

--- Srcmain/scala/syntax/syntax.scala
+++ Srcmain/scala/syntax/syntax.scala
@@ -101,3 +101,4 @@ sealed abstract class Proc extends SyntaxElement {
     case LetIn      ( n , t  , e  , p      ) =>
       e.free union (p.free - n) union t.free
     case Send       ( c , ts , ms , p      ) => c.free union p.free union
+      ( ( ms map ( _.free ) ) ++ ( ts map ( _.free ) ) )
